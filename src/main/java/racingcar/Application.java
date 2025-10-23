0dd567326ea42d43ac80f10f37bae9915497c752
ibtSdan <ibtsdan@gmail.com>
2025-10-23 19:08:37 +0900

    feat: Application main() 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.factory.CarFactory;
+import racingcar.input.InputHandler;
+import racingcar.input.InputProvider;
+import racingcar.validator.CarNameValidator;
+import racingcar.validator.TryCountValidator;
+import racingcar.view.RacingCarView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarFactory factory = new CarFactory();
+        CarNameValidator carNameValidator = new CarNameValidator();
+        TryCountValidator tryCountValidator = new TryCountValidator();
+        InputProvider provider = new InputHandler();
+        RacingCarView view = new RacingCarView(provider);
+
+        RacingCarController controller = new RacingCarController(factory, carNameValidator, tryCountValidator, view);
+        controller.run();
     }
 }
